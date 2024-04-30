696999f372aaa4d9c3a1a94e25023a1dbae00e74
unknown <petarbonev13@gmail.com>
2024-04-30 12:35:03 +0300

    program.cs

--- PetShelter/Program.cs
+++ PetShelter/Program.cs
@@ -2,25 +2,18 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace PetShelter
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+builder.Services.AddControllerWithViews();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-}
+builder.Services.AddDbContext<PetShelterDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration["ConnectionString:DefaultConnection"]);
+});
+
+var app = builder.Build();
+     
