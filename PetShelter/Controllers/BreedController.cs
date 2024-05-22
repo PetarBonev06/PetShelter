5a825ab571cad70b6d5ef11c10c797e061889897
unknown <petarbonev13@gmail.com>
2024-05-22 10:16:39 +0300

    Create 5 cshtml files

--- PetShelter/Controllers/BreedController.cs
+++ PetShelter/Controllers/BreedController.cs
@@ -1,17 +1,17 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
-using PetShelter.Services;
 using PetShelter.Shared.Dtos;
 using PetShelter.Shared.Repos.Contracts;
+using PetShelter.Shared.Services.Contracts;
 using PetShelter.ViewModels;
 
 namespace PetShelter.Controllers
 {
     [Authorize(AuthenticationSchemes = CookieAuthenticationDefaults.AuthenticationScheme, Roles = "Admin, Employee")]
-    public class BreedController : BaseCrudController<BreedDto, IBreedRepository, IBreedService, BreedEditVM, BreedDetailsVM>
+    public class BreedController : BaseCrudController<BreedDto, IBreedRepository, IBreedsService, BreedEditVM, BreedDetailsVM>
     {
-        public BreedController(IBreedService service, IMapper mapper) : base(service, mapper)
+        public BreedController(IBreedsService service, IMapper mapper) : base(service, mapper)
         {
         }
     }
---
+++ PetShelter/Views/Auth/Breed/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PetShelter.Views.Auth.Breed
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
