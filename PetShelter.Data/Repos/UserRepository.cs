f8b8f993c9e5dcbe3294029b1175a1925cfd3283
unknown <petarbonev13@gmail.com>
2024-05-29 09:17:41 +0300

    Added Location,Pet,PetType htmls

--- PetShelter.Data/Repos/UserRepository.cs
+++ PetShelter.Data/Repos/UserRepository.cs
@@ -2,9 +2,11 @@
 using PetShelter.Shared.Dtos;
 using PetShelter.Shared.Repos.Contracts;
 using PetShelter.Data.Entities;
+using PetShelter.Shared.Attributes;
 
 namespace PetShelter.Data.Repos
 {
+    [AutoBind]
     public class UserRepository : BaseRepository<User, UserDto>, IUserRepository
     {
         public UserRepository(PetShelterDbContext context, IMapper mapper) : base(context, mapper)
