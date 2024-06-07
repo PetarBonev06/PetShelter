04f256153fcae26352c21224cffc2ae038dc4def
unknown <petarbonev13@gmail.com>
2024-06-07 13:18:09 +0300

    added RepositoryTests and 2 ServiceTests

---
+++ PetShelter.Tests/Repos/UserRepositoryTests.cs
@@ -0,0 +1,14 @@
+﻿using PetShelter.Data.Repos;
+using PetShelter.Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShelter.Tests.Repos
+{
+    public class UserRepositoryTests : BaseRepositoryTests<UserRepository, PetShelter.Data.Entities.User, UserDto>
+    {
+    }
+}
